b33311f8735e81956d20c1ff041717b61bbc19c9
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2026-07-24 00:50:36 +0100

    finished grpc test

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	"github.com/michaelNuel/demo-grpc/invoicer"
+	"google.golang.org/grpc"
 )
 
-func main() {
-	lis, err :=net.Listen("tcp", ":8080")
-	if err != nil {
+type myInvoicerServer struct { 
+  invoicer.UnimplementedInvoicerServer
+} 
+
+func (s myInvoicerServer ) Create(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+  return &invoicer.CreateResponse{
+	Pdf: []byte("pdf content"), 
+	Docx: []byte("docx content"),
+  }, nil 
+}
+
+func main() { 
+	lis, err :=net.Listen("tcp", ":8080") 
+	if err != nil { 
 		log.Fatalf("cannot create listener %s", err)
-	}
-	invoicer.RegisterInvoicerServer()
-}  
+	} 
+
+	serverRegister := grpc.NewServer()
+	service := &myInvoicerServer{} 
+	invoicer.RegisterInvoicerServer(serverRegister, service)
+    err = 	serverRegister.Serve(lis)
+    if err != nil {
+	  log.Fatalf("cannot serve %s", err)
+   }
+}   
+
